650e1257e80ae397fb78d9087cd72db55b2ab93f
Lucas Samuel <lucasskluser@outlook.com>
2020-01-05 21:04:20 -0300

    Implementação do role manager e startup do bot

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 import { Client } from "discord.js";
 import { loadEvents, loadCommands } from './core/managers/eventManager';
 
-const path = require('path');
 const client = new Client({ partials: ["CHANNEL", "MESSAGE"] });
+const path = require('path');
 
 client.login(process.env.TOKEN);
 
 const eventsDir = path.join(__dirname, 'events');
 const commandsDir = path.join(__dirname, 'commands');
+
 loadEvents(eventsDir, client);
 loadCommands(commandsDir, client);
