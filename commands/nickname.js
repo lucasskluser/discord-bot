1240e977a716cd42db32292a51247359c84008c3
Lucas Samuel <lucasskluser@outlook.com>
2020-01-06 01:59:24 -0300

    Refatoração

--- commands/nickname.js
+++ commands/nickname.js
@@ -2,15 +2,14 @@ module.exports = (client, message) => {
   if (!(message.channel.type === "dm")) return;
 
   const nick = message.content.split(" ")[1];
-  const guild = client.guilds.get("662399736724324362");
+  const guild = client.guilds.get(process.env.GUILD_ID);
   const member = guild.members.get(message.author.id);
 
   member
     .setNickname(nick)
-    .then(console.log(`Nickname de ${message.author.username} alterado`))
     .then(message.channel.send(`O seu apelido foi alterado para **${nick}**.`))
     .catch(err => {
       console.log(err);
-      // message.channel.send('Ops, não consegui alterar o seu apelido. :confused:');
+      message.channel.send('Ops, não consegui alterar o seu apelido. :confused:');
     });
 };
