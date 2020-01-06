1bab0d8b16bae2d8b18a43baf5b9e7040744dac8
Lucas Samuel <lucasskluser@outlook.com>
2020-01-06 01:48:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
+/**
+ * Arquivo de inicialização da aplicação
+ * 
+ * A aplicação é iniciada e programada para escutar a porta
+ * definida nas variáveis de ambiente.
+ * 
+ * Após isso, os eventos e os comandos do bot são registrados.
+ * 
+ * @since 03/01/2020
+ * @version 1.2.4
+ * @author Lucas Samuel Kluser
+ */
+
 require('dotenv').config();
 
+/**
+ * Inicialização do servidor express embutido
+ * do NodeJS para manter a aplicação online
+ */
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.send('hello world'); 
+});
+
+app.listen(port);
+
+/**
+ * Inicialização dos registradores de eventos
+ * e comandos do bot
+ */
 const Discord = require("discord.js");
 const EventManager = require('./core/eventManager');
 
