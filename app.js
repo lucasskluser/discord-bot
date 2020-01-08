0a1361cd13d3f3bc36829d76a4873701ae5b041d
Lucas Samuel <lucasskluser@outlook.com>
2020-01-08 16:42:00 -0300

    add(env): configuração do Sentry
    Adicionada variável de ambiente para configuração do Sentry; corrigida documentação.

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 /**
  * Arquivo de inicialização da aplicação
- * 
+ *
  * A aplicação é iniciada e programada para escutar a porta
  * definida nas variáveis de ambiente.
- * 
+ *
  * Após isso, os eventos e os comandos do bot são registrados.
- * 
+ *
  * @since 03/01/2020
- * @version 1.2.4
+ * @version 1.2.1
  * @author Lucas Samuel Kluser
  */
 
-require('dotenv').config();
+require("dotenv").config();
 
 /**
  * Inicialização do monitoramento de exceções
  * do Sentry
  */
-const Sentry = require('@sentry/node');
-Sentry.init({ dsn: 'https://62761952009d40b4aabd06d01de10411@sentry.io/1875279' });
+const Sentry = require("@sentry/node");
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 /**
  * Inicialização do servidor express embutido
@@ -28,23 +28,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    res.sendStatus(200);
+app.get("/", function(req, res) {
+  res.sendStatus(200);
 });
 
 app.listen(port);
 
 /**
  * Autoping para manter o servidor ativo
- * 
+ *
  * Realiza uma requisição HTTP GET a cada 29 minutos
  * para manter o servidor online no Heroku
  */
-const https = require('https');
+const https = require("https");
 
 setInterval(() => {
-    console.log('Pinging to keep alive...');
-    https.get(`https://anthem-bot-br.herokuapp.com/`);
+  console.log("Pinging to keep alive...");
+  https.get(`https://anthem-bot-br.herokuapp.com/`);
 }, 1740000);
 
 /**
@@ -52,7 +52,7 @@ setInterval(() => {
  * e comandos do bot
  */
 const Discord = require("discord.js");
-const EventManager = require('./core/eventManager');
+const EventManager = require("./core/eventManager");
 
 const client = new Discord.Client({ partials: ["CHANNEL", "MESSAGE"] });
 client.login(process.env.TOKEN);
