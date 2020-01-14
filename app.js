fbb4ccd882ca0ed4dc77c609de1d10e39e07bc4f
Lucas Samuel <lucasskluser@outlook.com>
2020-01-14 16:55:55 -0300

    improvement: configurações de ambiente de execução

--- app.js
+++ app.js
@@ -11,50 +11,74 @@
  * @author Lucas Samuel Kluser
  */
 
+const path = require("path");
 require("dotenv").config();
 
-/**
- * Inicialização do monitoramento de exceções
- * do Sentry
- */
-const Sentry = require("@sentry/node");
-Sentry.init({ dsn: process.env.SENTRY_DSN });
+console.log(`Aplicação iniciada em ${process.env.NODE_ENV}`)
 
-/**
- * Inicialização do servidor express embutido
- * do NodeJS para manter a aplicação online
- */
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV === "production") {
+  /**
+   * Inicialização do monitoramento de exceções
+   * do Sentry
+   */
+  const Sentry = require("@sentry/node");
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
 
-app.get("/", function(req, res) {
-  res.sendStatus(200);
-});
+  /**
+   * Inicialização do servidor express embutido
+   * do NodeJS para manter a aplicação online
+   */
+  const express = require("express");
+  const app = express();
+  const port = process.env.PORT || 3000;
 
-app.listen(port);
+  app.get("/", function(req, res) {
+    res.sendStatus(200);
+  });
 
-/**
- * Autoping para manter o servidor ativo
- *
- * Realiza uma requisição HTTP GET a cada 29 minutos
- * para manter o servidor online no Heroku
- */
-const https = require("https");
+  app.listen(port);
 
-setInterval(() => {
-  console.log("Pinging to keep alive...");
-  https.get(`https://anthem-bot-br.herokuapp.com/`);
-}, 1740000);
+  /**
+   * Autoping para manter o servidor ativo
+   *
+   * Realiza uma requisição HTTP GET a cada 29 minutos
+   * para manter o servidor online no Heroku
+   */
+  const https = require("https");
+
+  setInterval(() => {
+    console.log("Pinging to keep alive...");
+    https.get(`https://anthem-bot-br.herokuapp.com/`);
+  }, 1740000);
+}
 
 /**
  * Inicialização dos registradores de eventos
  * e comandos do bot
  */
-const Discord = require("discord.js");
-const EventManager = require("./core/eventManager");
+const { CommandoClient } = require("discord.js-commando");
 
-const client = new Discord.Client({ partials: ["CHANNEL", "MESSAGE"] });
+const client = new CommandoClient({
+  commandPrefix: "!",
+  owner: "352932842944069643",
+  invite: "https://discord.gg/xgmEGg",
+  unknownCommandResponse: false
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ["utils", "Utilidade"],
+    ["user", "Para usuários"]
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    help: false,
+    prefix: false
+  })
+  .registerCommandsIn(path.join(__dirname, "commands"));
+
 client.login(process.env.TOKEN);
 
+const EventManager = require("./core/eventManager");
 EventManager(client, __dirname);
